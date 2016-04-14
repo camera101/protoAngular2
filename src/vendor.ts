e832cadf2c95e0fe632de8f62373110728d38ae8
alexandruco <alexandruco@fortech.ro>
2016-04-14 10:48:25 +0300

    Library upgrades. Everything works fine.

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,7 +1,3 @@
-import 'es6-shim/es6-shim.js';
-import './shims/shims_for_IE.js';
-import 'angular2/bundles/angular2-polyfills.js';
-
 import 'angular2-jwt';
 import 'jwt-decode/standalone.js';
 
@@ -13,4 +9,5 @@ import 'angular2/platform/common_dom';
 import 'angular2/core';
 import 'angular2/router';
 import 'angular2/http';
+
 import 'rxjs';
